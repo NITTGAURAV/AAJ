20cab09968b661c25225e42c213d08d05126d4a8
NITTGAURAV <124367117+NITTGAURAV@users.noreply.github.com>
2023-02-03 18:56:22 +0530

    Add files via upload

---
+++ Untitled22.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+a=[1,2,"daur",4,6,[2,5,8],3,9]
+
+
+# In[7]:
+
+
+a[5].remove(5)
+
+
+# In[8]:
+
+
+a
+
+
+# In[9]:
+
+
+a[2].remove(ur)
+
+
+# In[1]:
+
+
+a=[3,4,5,6,7]
+
+
+# In[2]:
+
+
+a[::-1]
+
+
+# In[3]:
+
+
+a
+
+
+# In[4]:
+
+
+a.reverse()
+
+
+# In[5]:
+
+
+a
+
+
+# In[7]:
+
+
+j={2:"gaurav",True:65}
+
+
+# In[8]:
+
+
+j[2]
+
+
+# In[9]:
+
+
+j[1]
+
+
+# In[1]:
+
+
+a={2:"gaurav","hand":[3, 7,8]}
+
+
+# In[2]:
+
+
+a["hand"][1]
+
+
+# In[2]:
+
+
+a=["gaurav","saurav","chandni"]
+b=[]
+for i in a:
+    print(i)
+    b.append(i.upper())
+
+
+# In[3]:
+
+
+b
+
+
+# In[10]:
+
+
+k=[]
+m=[]
+c=["hon",34,"gau",65]
+for v in c:
+    if type(v)==int or type(v)==float:
+        k.append(v)
+    else:
+        m.append(v)
+    
+
+
+# In[11]:
+
+
+k
+
+
+# In[ ]:
+
+
+
+
